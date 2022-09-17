7dd502c69cec62bb5935ee1a545ac2eab02aac2c
Homin Lee <ff4500@gmail.com>
2022-09-17 15:11:39 +0900

    add moved anounce page

--- deploy_gh-pages.sh
+++ deploy_gh-pages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#hugo
-rm -rf public
-cp index.html public/
-cd public
+rm -rf gh-pages
+mkdir gh-pages
+cp index.tmpl gh-pages/index.html
+cd gh-pages
 git init . && git add . && git commit -m "update at `date`"
 git push -f \
 	git@github.com:suapapa/blog.git \

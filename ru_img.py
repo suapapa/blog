2290327b28ed2851cae3c88f15fdb37ea034d24f
Homin Lee <homin.lee@suapapa.net>
2024-01-27 23:28:21 +0900

    ru_img.py: process multiple files

--- ru_img.py
+++ ru_img.py
@@ -9,23 +9,24 @@ if __name__ == '__main__':
                         help='set positive size to make thumbnail')
     psr.add_argument('-u', dest='upload', type=bool, default=False,
                         help='upload image to gcs')
-    psr.add_argument('file', help='image file to resize or upload')
-    args = psr.parse_args()
+    # psr.add_argument('file', help='image file to resize or upload')
+    args, arg_files = psr.parse_known_args()
 
-    r = ""
-    if args.resize_size > 0:
-        f, e = os.path.splitext(args.file)
-        r = os.path.basename(f) + f'_{args.resize_size}.jpg'
-        os.system(f'convert {args.file} -resize {args.resize_size} {r}')
-        print(f'made {r}')
+    for f in arg_files:
+        r = ""
+        if args.resize_size > 0:
+            n, e = os.path.splitext(f)
+            r = os.path.basename(n) + f'_{args.resize_size}.jpg'
+            os.system(f'convert {f} -resize {args.resize_size} {r}')
+            print(f'made {r}')
 
-    if args.upload:
-        u, m = "", ""
-        if r:
-            u, b = r, r
-        else:
-            u, b = args.file, os.path.basename(args.file)
-        
-        os.system(f'gsutil cp {u} gs://homin-dev_asset/blog/img/{b}')
-        print(f'use ![{b}](https://homin.dev/asset/blog/img/{b})')
+        if args.upload:
+            u, m = "", ""
+            if r:
+                u, b = r, r
+            else:
+                u, b = args.file, os.path.basename(f)
+            
+            os.system(f'gsutil cp {u} gs://homin-dev_asset/blog/img/{b}')
+            print(f'use ![{b}](https://homin.dev/asset/blog/img/{b})')
     
